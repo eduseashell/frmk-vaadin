15ba1bda1db5a7ff48c833f37da31f78b13f9dfd
seashell <edu.android.2015+git@gmail.com>
2015-06-03 07:46:39 +0700

    Initial project start up

---
+++ src/main/java/edu/kwon/frmk/vaadin/component/select/SimpleTable.java
@@ -0,0 +1,40 @@
+package edu.kwon.frmk.vaadin.component.select;
+
+import java.util.List;
+
+import com.vaadin.ui.Table;
+import com.vaadin.ui.themes.ValoTheme;
+
+public class SimpleTable extends Table {
+	
+	private static final long serialVersionUID = 5884533621024236413L;
+	
+	private static int defaultPageLength = 10;
+
+	public SimpleTable(String caption) {
+		super();
+		setCaption(caption);
+		addStyleName(ValoTheme.TABLE_COMPACT);
+		setSelectable(true);
+		setEditable(false);
+		setNullSelectionAllowed(false);
+		setPageLength(defaultPageLength);
+		setSizeFull();
+	}
+	
+	public void addColumns(List<Column> columns) {
+		if (columns != null) {
+			for (Column column : columns) {
+				addColumn(column);
+			}
+		}
+	}
+	
+	private void addColumn(Column column) {
+		addContainerProperty(column.getPropertyId(), column.getClazz(), null);
+		setColumnHeader(column.getPropertyId(), column.getColumnHeader());
+		setColumnAlignment(column.getPropertyId(), column.getAlign());
+		setColumnWidth(column.getPropertyId(), column.getWidth());
+	}
+
+}
